25c7b26999fcd656a43fc45cb8ded91daccf06d6
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-01-18 17:25:07 -0300

    corrige erro de buildpath e retira imports desnecessarios

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationService.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationService.java
@@ -1,11 +1,9 @@
 package dev.wakandaacademy.produdoro.usuario.application.service;
 
-import javax.validation.Valid;
+import java.util.UUID;
 
 import dev.wakandaacademy.produdoro.usuario.application.api.UsuarioCriadoResponse;
 import dev.wakandaacademy.produdoro.usuario.application.api.UsuarioNovoRequest;
-
-import java.util.UUID;
 
 public interface UsuarioApplicationService {
 	UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo);
