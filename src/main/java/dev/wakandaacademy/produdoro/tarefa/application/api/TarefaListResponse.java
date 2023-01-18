cea67ef2ad987e2ad16ac4bb3105131fb62e6af8
Matheus Fagundes <matewsfagundes@gmail.com>
2023-01-18 20:56:25 -0300

    implementa camada de API e Controller

---
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaListResponse.java
@@ -0,0 +1,18 @@
+package dev.wakandaacademy.produdoro.tarefa.application.api;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusAtivacaoTarefa;
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusTarefa;
+import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+
+public class TarefaListResponse {
+
+	private UUID idTarefa;
+	private String descricao;
+	private StatusTarefa status;
+	private StatusAtivacaoTarefa statusAtivacao;
+	private int contagemPomodoro;
+
+}
