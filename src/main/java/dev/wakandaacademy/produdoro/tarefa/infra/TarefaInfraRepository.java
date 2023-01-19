7cd725126a318532fed9773a25c1fb64b6ceb585
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-01-19 16:43:44 -0300

    implementa camada tarefaInfraRepository

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/infra/TarefaInfraRepository.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/infra/TarefaInfraRepository.java
@@ -1,16 +1,21 @@
 package dev.wakandaacademy.produdoro.tarefa.infra;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+
 import dev.wakandaacademy.produdoro.handler.APIException;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 @Log4j2
@@ -18,6 +23,7 @@ import java.util.UUID;
 public class TarefaInfraRepository implements TarefaRepository {
 
     private final TarefaSpringMongoDBRepository tarefaSpringMongoDBRepository;
+    private final MongoTemplate mongoTemplate;
 
     @Override
     public Tarefa salva(Tarefa tarefa) {
@@ -38,6 +44,12 @@ public class TarefaInfraRepository implements TarefaRepository {
     }
 	@Override
 	public void desativaTarefas(UUID idUsuario) {
-		
+		log.info("[inicia] TarefaInfraRepository - desativaTarefas");
+		Query query = new Query();
+        query.addCriteria(Criteria.where("idUsuario").is(idUsuario));
+        Update update = new Update();
+        update.set("statusAtivacao", "INATIVA");
+        mongoTemplate.updateMulti(query, update, Tarefa.class);
+		log.info("[finaliza] TarefaInfraRepository - desativaTarefas");
 	}
 }
